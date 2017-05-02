5630d02ad870f144116cea1df9c73812ceea1e53
Vaeb <vaebmail@gmail.com>
2017-05-02 21:08:35 +0100

    Fixed function bug

--- Util.js
+++ Util.js
@@ -217,7 +217,7 @@ exports.getURLChecker = function() {
 			punct_regexp: /(?:[!?.,:;'"]|(?:&|&amp;)(?:lt|gt|quot|apos|raquo|laquo|rsaquo|lsaquo);)$/
 		};
 
-	exports.checkURLs = function( txt, options ) {
+	checkURLs = function( txt, options ) {
 		txt = txt.replaceAll("\\", "");
 		txt = txt.replaceAll("*", "");
 		txt = txt.replaceAll("_", "");
