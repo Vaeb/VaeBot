659caa81e8685fbf6a76c92835ce5d6f2271ac10
Vaeb <vaebmail@gmail.com>
2017-05-06 21:31:09 +0100

    Added Stop command

---
+++ commands/staff/Stop.js
@@ -0,0 +1,33 @@
+module.exports = Cmds.addCommand({
+	cmds: [";stop", ";silence"],
+
+	requires: {
+		guild: true,
+		loud: false
+	},
+
+	desc: "Cancel the party, the bangin' tunes can wait for another day",
+
+	args: "[song_name]",
+
+	example: "gonna give you up",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		var realSongs = Music.songs[guild.id];
+		var realSongData = Music.songData[guild.id];
+		var connection = guild.voiceConnection;
+		if (connection) {
+			if (Music.isPlaying[guild.id]) {
+				if (realSongData.isAuto == false) realSongs.splice(0, 1);
+				Music.stopMusic(guild);
+				Util.print(channel, "Stopping audio");
+			} else {
+				Util.print(channel, "No audio playing");
+			}
+		} else {
+			Util.print(channel, "No audio found");
+		}
+	}
+});
