835f52d6d0569cd45972ac3144aa74206c16caa4
Vaeb <vaebmail@gmail.com>
2017-05-06 21:30:23 +0100

    Added ClearQueue command

---
+++ commands/staff/ClearQueue.js
@@ -0,0 +1,21 @@
+module.exports = Cmds.addCommand({
+	cmds: [";clearqueue"],
+
+	requires: {
+		guild: true,
+		loud: false
+	},
+
+	desc: "Clears VaeBot's queue of music",
+
+	args: "",
+
+	example: "",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		Music.clearQueue(guild);
+		Util.print(channel, "Cleared queue");
+	}
+});
