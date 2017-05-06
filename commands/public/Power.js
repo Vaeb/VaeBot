93e5b2dccdb97e2606cf9f767f765fb02b3549e0
Vaeb <vaebmail@gmail.com>
2017-05-06 12:45:07 +0100

    Added Power command

---
+++ commands/public/Power.js
@@ -0,0 +1,33 @@
+module.exports = Cmds.addCommand({
+	cmds: [";rank ", ";rate ", ";power "],
+
+	requires: {
+		guild: true,
+		loud: false
+	},
+
+	desc: "Are you over 9000?!",
+
+	args: "",
+
+	example: "",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		var target = Util.getMemberByMixed(args, guild);
+		if (target == null) return Util.commandFailed(channel, speaker, "User not found");
+
+		var highestRole = Util.getHighestRole(target);
+		var powerRating = toFixed(Util.getPermRating(guild, target), 3) + "%";
+
+		var sendEmbedFields = [];
+
+		sendEmbedFields.push({name: "Username", value: target.toString()});
+		sendEmbedFields.push({name: "Staff", value: Util.capitalize(Util.checkStaff(guild, target))});
+		sendEmbedFields.push({name: "Rank", value: highestRole.name + " (" + highestRole.position + ")"});
+		sendEmbedFields.push({name: "Power", value: powerRating});
+
+		Util.sendEmbed(channel, "User Power Ranking", null, Util.makeEmbedFooter(speaker), Util.getAvatar(target), 0x00E676, sendEmbedFields);
+	}
+});
