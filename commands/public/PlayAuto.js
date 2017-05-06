77b495048df6c7ee2c743506f4c6558f3e0ea48f
Vaeb <vaebmail@gmail.com>
2017-05-06 21:29:11 +0100

    Added PlayAuto command

---
+++ commands/public/PlayAuto.js
@@ -0,0 +1,34 @@
+module.exports = Cmds.addCommand({
+	cmds: [";playauto ", ";playa "],
+
+	requires: {
+		guild: true,
+		loud: false
+	},
+
+	desc: "Plays a tune already stored in the auto-playlist",
+
+	args: "[song_name]",
+
+	example: "gonna give you up",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		args = args.toLowerCase();
+		var autoPlaylist = Data.guildGet(guild, Data.playlist);
+		var autoSongs = autoPlaylist.songs;
+		for (var i = 0; i < autoSongs.length; i++) {
+			var newSong = autoSongs[i];
+			var video = newSong[0];
+			var author = newSong[1];
+			var title = video.snippet.title;
+			if (title.toLowerCase().indexOf(args) >= 0) {
+				Music.joinMusic(guild, channel, (connection) => {
+					Music.addSong(speaker, guild, channel, video);
+				});
+				break;
+			}
+		}
+	}
+});
