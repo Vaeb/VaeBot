a775bf5df9070ab1d5180e1bc91a85185e5c6bd5
Vaeb <vaebmail@gmail.com>
2017-05-06 12:47:30 +0100

    Added Text command

---
+++ commands/public/Text.js
@@ -0,0 +1,67 @@
+var charToId = {
+	"a": "244832142956298240",
+	"b": "244832158290673664",
+	"c": "244832181501820928",
+	"d": "244832192197296128",
+	"e": "244832201395535873",
+	"f": "244832212535607296",
+	"g": "244832220513042432",
+	"h": "244832228880678913",
+	"i": "244832236463980545",
+	"j": "244832243342639106",
+	"k": "244832250963820545",
+	"l": "244832258274492416",
+	"m": "244832265270591489",
+	"n": "244832272253976577",
+	"o": "244832280281874433",
+	"p": "244832289052164096",
+	"q": "244832298552262657",
+	"r": "244832306878087169",
+	"s": "244832315174420480",
+	"t": "244832323529342976",
+	"u": "244832374343335936",
+	"v": "244832383998754818",
+	"w": "244832392873771019",
+	"x": "244832402856214543",
+	"y": "244832411366588416",
+	"z": "244832419079782400",
+};
+
+module.exports = Cmds.addCommand({
+	cmds: [";txt ", ";text ", ";type "],
+
+	requires: {
+		guild: false,
+		loud: false
+	},
+
+	desc: "Echo your text with emojis",
+
+	args: "([message])",
+
+	example: "hello there",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		var str = "";
+		var wasChar = false;
+		for (var i = 0; i < args.length; i++) {
+			var char = args[i];
+			var charl = char.toLowerCase();
+			if (charToId.hasOwnProperty(charl)) {
+				char = "<:" + charl + "_:" + charToId[charl] + ">";
+				if (wasChar) {
+					char = " " + char;
+					wasChar = false;
+				}
+			} else {
+				wasChar = true;
+			}
+			str += char;
+		}
+		//str = str.replace(/@/g, "@​");
+		str = Util.safe(str);
+		Util.print(channel, str);
+	}
+});
