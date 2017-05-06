9f771ffd61999b18274b835f09783756aebc8b15
Vaeb <vaebmail@gmail.com>
2017-05-06 21:30:30 +0100

    Added Leave command

---
+++ commands/staff/Leave.js
@@ -0,0 +1,25 @@
+module.exports = Cmds.addCommand({
+	cmds: [";leave", ";exit"],
+
+	requires: {
+		guild: true,
+		loud: false
+	},
+
+	desc: "Make VaeBot leave it's voice channel",
+
+	args: "",
+
+	example: "",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		Music.clearQueue(guild);
+		var connection = guild.voiceConnection;
+		if (!connection) return Util.commandFailed(channel, speaker, "Not in a voice channel");
+		var voiceChannel = connection.channel;
+		voiceChannel.leave();
+		Util.sendDescEmbed(channel, "Left Voice Channel", voiceChannel.name, Util.makeEmbedFooter(speaker), null, 0x00E676);
+	}
+});
