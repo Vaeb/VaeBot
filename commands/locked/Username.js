93fc78d5ae21a35ed147a320e57a850641a6660c
Vaeb <vaebmail@gmail.com>
2017-05-06 21:27:29 +0100

    Added Username command

---
+++ commands/locked/Username.js
@@ -0,0 +1,22 @@
+module.exports = Cmds.addCommand({
+	cmds: [";username "],
+
+	requires: {
+		guild: false,
+		loud: false
+	},
+
+	desc: "Set VaeBot's username",
+
+	args: "[username]",
+
+	example: "VaeBot9000",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		client.user.setUsername(args)
+			.catch(console.error);
+		Util.print(channel, "Set username to " + args);
+	}
+});
