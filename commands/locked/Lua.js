2d5dc0028f966004a44c4d5da0673bab657662d6
Vaeb <vaebmail@gmail.com>
2017-05-06 21:26:42 +0100

    Added Lua command

---
+++ commands/locked/Lua.js
@@ -0,0 +1,31 @@
+module.exports = Cmds.addCommand({
+	cmds: [";lua "],
+
+	requires: {
+		guild: false,
+		loud: false
+	},
+
+	desc: "Execute Lua code",
+
+	args: "[code]",
+
+	example: "print(\"Hello, world!\")",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		if (args.substr(0, 3) == "-h ") {
+			var url = args.substring(3);
+			index.Request(url, function(error, response, body) {
+				if (error) {
+					Util.print(channel, "[HTTP]", error);
+				} else {
+					Util.runLua(body, channel);
+				}
+			});
+		} else {
+			Util.runLua(args, channel);
+		}
+	}
+});
