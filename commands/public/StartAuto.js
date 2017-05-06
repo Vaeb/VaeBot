7b208b7602d8decb0145b676c1694e16f2e228b1
Vaeb <vaebmail@gmail.com>
2017-05-06 21:29:31 +0100

    Added StartAuto command

---
+++ commands/public/StartAuto.js
@@ -0,0 +1,28 @@
+module.exports = Cmds.addCommand({
+	cmds: [";startauto", ";startap"],
+
+	requires: {
+		guild: true,
+		loud: false
+	},
+
+	desc: "Start playing the auto-playlist music",
+
+	args: "",
+
+	example: "",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		Music.joinMusic(guild, channel, connection => {
+			var autoPlaylist = Data.guildGet(guild, Data.playlist);
+			if (autoPlaylist.hasOwnProperty("songs") && autoPlaylist.songs.length > 0) {
+				console.log("Cmd, Playing Next Auto");
+				Music.playNextAuto(guild, channel, true);
+			} else {
+				Util.print(channel, "No songs in the auto-playlist");
+			}
+		});
+	}
+});
