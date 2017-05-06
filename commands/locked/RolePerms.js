9f76c4eb559bf79a55f579d28c489aadd647718c
Vaeb <vaebmail@gmail.com>
2017-05-06 21:27:20 +0100

    Added RolePerms command

---
+++ commands/locked/RolePerms.js
@@ -0,0 +1,28 @@
+module.exports = Cmds.addCommand({
+	cmds: [";roleperms", ";rolepermissions", ";gperms"],
+
+	requires: {
+		guild: true,
+		loud: false
+	},
+
+	desc: "Get all role permissions",
+
+	args: "",
+
+	example: "",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		var outStr = [];
+		outStr.push("**Guild permissions:**\n```");
+		guild.roles.forEach(function(value, index, self) {
+			outStr.push("Role: " + value.name + " (" + value.id + ")");
+			outStr.push(JSON.stringify(value.serialize()));
+			outStr.push("");
+		});
+		outStr.push("```");
+		Util.print(channel, outStr.join("\n"));
+	}
+});
