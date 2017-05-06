79528db11ba5d94f05bd6410f38c8765373bd141
Vaeb <vaebmail@gmail.com>
2017-05-06 21:27:04 +0100

    Added PSA command

---
+++ commands/locked/PSA.js
@@ -0,0 +1,20 @@
+module.exports = Cmds.addCommand({
+	cmds: [";psa"],
+
+	requires: {
+		guild: false,
+		loud: false
+	},
+
+	desc: "PSA when restarting the bot for an update",
+
+	args: "",
+
+	example: "",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		Util.sendDescEmbed(channel, null, "Restarting the bot for an update, it will be down for a few seconds", null, null, null);
+	}
+});
