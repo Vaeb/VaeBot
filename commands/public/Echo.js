8af32720391a3763b3b175f53cac1782e902ef7d
Vaeb <vaebmail@gmail.com>
2017-05-06 21:28:02 +0100

    Added Echo command

---
+++ commands/public/Echo.js
@@ -0,0 +1,35 @@
+module.exports = Cmds.addCommand({
+	cmds: [";echo "],
+
+	requires: {
+		guild: false,
+		loud: false
+	},
+
+	desc: "Echo text into another channel",
+
+	args: "",
+
+	example: "",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		var channels = client.channels;
+		var data = Util.getDataFromString(args, [
+			function(str, results) {
+				var newChannel = channels.get(str);
+				return newChannel;
+			},
+			function(str, results) {
+				return str;
+			}
+		], false);
+		if (!data) return Util.commandFailed(channel, speaker, "User not found");
+		//msgObj.delete();
+		var newChannel = data[0];
+		var msg = data[1];
+		console.log("echo'd " + msg);
+		Util.print(newChannel, msg);
+	}
+});
