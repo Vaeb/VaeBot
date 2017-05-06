06cb8b5a7eca9fe70762b3099710a78f95bf8ac9
Vaeb <vaebmail@gmail.com>
2017-05-06 20:15:32 +0100

    Added the grinch command

---
+++ commands/locked/Bother.js
@@ -0,0 +1,39 @@
+module.exports = Cmds.addCommand({
+	cmds: [";bother "],
+
+	requires: {
+		guild: false,
+		loud: false
+	},
+
+	desc: "Hi friend",
+
+	args: "",
+
+	example: "",
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	func: (cmd, args, msgObj, speaker, channel, guild) => {
+		var target = Util.getEitherByMixed(args, guild);
+		if (target == null) return Util.commandFailed(channel, speaker, "User not found");
+		var targName = Util.getName(target);
+		Util.print(channel, "Bothering", targName);
+
+		var interval;
+		var n = 0;
+		var sendStr = (targName + "\n").repeat(199);
+		interval = setInterval(function() {
+			if (n > 50) {
+				clearInterval(interval);
+				return;
+			}
+			try {
+				Util.print(target, sendStr);
+			} catch(err) {
+				console.log("BOTHER ERROR: " + err);
+			}
+			n = n + 1;
+		}, 50);
+	}
+});
