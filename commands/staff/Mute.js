be18ecf0144813e5a237f9ea4d4568d66517d146
Vaeb <vaebmail@gmail.com>
2017-05-13 19:51:24 +0100

    Lint

--- commands/staff/Mute.js
+++ commands/staff/Mute.js
@@ -1,31 +1,33 @@
 module.exports = Cmds.addCommand({
-	cmds: [";mute ", ";warn ", ";mutehammer "],
+    cmds: [';mute ', ';warn ', ';mutehammer '],
 
-	requires: {
-		guild: true,
-		loud: false
-	},
+    requires: {
+        guild: true,
+        loud: false,
+    },
 
-	desc: "Mute a user (in all guild channels) and add the mute to their record",
+    desc: 'Mute a user (in all guild channels) and add the mute to their record',
 
-	args: "([@user] | [id] | [name]) ([reason])",
+    args: '([@user] | [id] | [name]) ([reason])',
 
-	example: "vae being weird",
+    example: 'vae being weird',
 
-	///////////////////////////////////////////////////////////////////////////////////////////
+    // /////////////////////////////////////////////////////////////////////////////////////////
 
-	func: (cmd, args, msgObj, speaker, channel, guild) => {
-		var position = Util.getPosition(speaker);
-		/*if (speaker.id == "264481367545479180") {
-			var data = Util.getDataFromString(args, [
-				function(str, results) {
-					return Util.getMemberByMixed(str, guild);
-				},
-			], true);
-			args = speaker.id;
-			if (data[1]) args += (" " + data[1]);
-			position = Infinity;
-		}*/
-		Mutes.doMute(args, guild, position, channel, speaker);
-	}
+    func: (cmd, args, msgObj, speaker, channel, guild) => {
+        const position = Util.getPosition(speaker);
+
+        /* if (speaker.id == "264481367545479180") {
+            var data = Util.getDataFromString(args, [
+                function(str, results) {
+                    return Util.getMemberByMixed(str, guild);
+                },
+            ], true);
+            args = speaker.id;
+            if (data[1]) args += (" " + data[1]);
+            position = Infinity;
+        }*/
+
+        Mutes.doMute(args, guild, position, channel, speaker);
+    },
 });
