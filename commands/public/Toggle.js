09d06fb74133e91be3603ee5d8600bbc7f97a60b
Vaeb <vaebmail@gmail.com>
2017-05-22 16:35:18 +0100

    Made toggle allow for multiple commands

--- commands/public/Toggle.js
+++ commands/public/Toggle.js
@@ -1,40 +1,42 @@
 module.exports = Cmds.addCommand({
-	cmds: [";toggle "],
+    cmds: [';toggle '],
 
-	requires: {
-		guild: true,
-		loud: false
-	},
+    requires: {
+        guild: true,
+        loud: false,
+    },
 
-	desc: "Toggle an autorole on the speaker",
+    desc: 'Toggle an autorole on the speaker',
 
-	args: "([auto_role_name])",
+    args: '([auto_role_name_1] .. [auto_role_name_n])',
 
-	example: "hire",
+    example: 'hire',
 
-	///////////////////////////////////////////////////////////////////////////////////////////
+    // /////////////////////////////////////////////////////////////////////////////////////////
 
-	func: (cmd, args, msgObj, speaker, channel, guild) => {
-		var prop = args.toLowerCase();
-		var guildAutoRoles = Data.guildGet(guild, Data.autoRoles);
-		if (!guildAutoRoles.hasOwnProperty(prop)) {
-			Util.commandFailed(channel, speaker, "AutoRole not found");
-			return;
-		}
-		var roleName = guildAutoRoles[prop];
-		var roleObj = Util.getRole(roleName, guild);
-		if (!Util.hasRole(speaker, roleObj)) {
-			speaker.addRole(roleObj);
-			let sendEmbedFields = [
-				{name: "Role Name", value: roleObj.name}
-			];
-			Util.sendEmbed(channel, "AutoRole Added", null, Util.makeEmbedFooter(speaker), Util.getAvatar(speaker), 0x00E676, sendEmbedFields);
-		} else {
-			speaker.removeRole(roleObj);
-			let sendEmbedFields = [
-				{name: "Role Name", value: roleObj.name}
-			];
-			Util.sendEmbed(channel, "AutoRole Removed", null, Util.makeEmbedFooter(speaker), Util.getAvatar(speaker), 0x00E676, sendEmbedFields);
-		}
-	}
+    func: (cmd, args, msgObj, speaker, channel, guild) => {
+        const props = args.toLowerCase();
+        const guildAutoRoles = Data.guildGet(guild, Data.autoRoles);
+        props.split(' ').forEach((prop) => {
+            if (!Object.prototype.hasOwnProperty.call(guildAutoRoles, prop)) {
+                Util.commandFailed(channel, speaker, 'AutoRole not found');
+                return;
+            }
+            const roleName = guildAutoRoles[prop];
+            const roleObj = Util.getRole(roleName, guild);
+            if (!Util.hasRole(speaker, roleObj)) {
+                speaker.addRole(roleObj);
+                const sendEmbedFields = [
+                    { name: 'Role Name', value: roleObj.name },
+                ];
+                Util.sendEmbed(channel, 'AutoRole Added', null, Util.makeEmbedFooter(speaker), Util.getAvatar(speaker), 0x00E676, sendEmbedFields);
+            } else {
+                speaker.removeRole(roleObj);
+                const sendEmbedFields = [
+                    { name: 'Role Name', value: roleObj.name },
+                ];
+                Util.sendEmbed(channel, 'AutoRole Removed', null, Util.makeEmbedFooter(speaker), Util.getAvatar(speaker), 0x00E676, sendEmbedFields);
+            }
+        });
+    },
 });
