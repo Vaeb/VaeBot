c0d66653622f1c4bacd60c28c2f33139005264d6
Vaeb <vaebmail@gmail.com>
2017-06-29 17:14:47 +0100

    Fix

--- commands/public/Translate.js
+++ commands/public/Translate.js
@@ -13,14 +13,14 @@ module.exports = Cmds.addCommand({
     example: 'Hola mis amigos',
 
     func: (cmd, args, msgObj, speaker, channel, guild) => {
-        index.Translate(args, { to: 'en' }.then((res) => {
+        index.Translate(args, { to: 'en' }).then((res) => {
             const embFields = [
                 { name: `[${res.from.language.iso}] Original`, value: res.from.text.value, inline: false },
                 { name: '[en] Translation', value: res.text, inline: false },
             ];
             Util.sendEmbed(channel, 'Translated', null, Util.makeEmbedFooter(speaker), null, 0x00E676, embFields);
         })
-        .catch(console.error));
+        .catch(console.error);
 
         return undefined;
     },
