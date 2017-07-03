733b285e15799045162087223d9d316be04d03c4
Vaeb <vaebmail@gmail.com>
2017-07-03 14:38:02 +0100

    Minor

--- commands/public/Decrypt.js
+++ commands/public/Decrypt.js
@@ -1,37 +1,37 @@
 module.exports = Cmds.addCommand({
-    cmds: [";decrypt "],
+    cmds: [';decrypt '],
 
     requires: {
         guild: false,
-        loud: false
+        loud: false,
     },
 
-    desc: "Decrypt text using One Time Pad",
+    desc: 'Decrypt text using One Time Pad',
 
-    args: "([encryption]) ([key])",
+    args: '([encryption]) ([key])',
 
-    example: "1000110100000110010111000000010111000011011111 1100010000100110000101100001010101101110111111",
+    example: '1000110100000110010111000000010111000011011111 1100010000100110000101100001010101101110111111',
 
-    ///////////////////////////////////////////////////////////////////////////////////////////
+    // /////////////////////////////////////////////////////////////////////////////////////////
 
     func: (cmd, args, msgObj, speaker, channel, guild) => {
-        var mix = args.split(" ");
-        var encBits = mix[0];
-        var keyBits = mix[1];
-        var msgBits = "";
-        var msgStr = "";
+        const mix = args.split(' ');
+        const encBits = mix[0];
+        const keyBits = mix[1];
+        let msgBits = '';
+        let msgStr = '';
 
         if (encBits == null || keyBits == null) {
-            Util.commandFailed(channel, speaker, "Required syntax: `;decrypt result key`");
+            Util.commandFailed(channel, speaker, 'Required syntax: `;decrypt result key`');
             return;
         }
 
-        for (var i = 0; i < encBits.length; i++) {
+        for (let i = 0; i < encBits.length; i++) {
             msgBits += Util.doXOR(encBits[i], keyBits[i]);
-            if ((i+1) % 8 == 0) {
-                msgStr += String.fromCharCode(parseInt(msgBits.substr(i-7, 8), 2).toString(10));
+            if ((i + 1) % 8 == 0) {
+                msgStr += String.fromCharCode(parseInt(msgBits.substr(i - 7, 8), 2).toString(10));
             }
         }
-        Util.print(channel, "Result:", Util.safe(msgStr));
-    }
+        Util.print(channel, `Result: ${msgStr}`);
+    },
 });
