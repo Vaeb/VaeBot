c8818b313e28bc638cbd8b9b5f851641bb58743e
Vaeb <vaebmail@gmail.com>
2017-07-03 22:15:37 +0100

    Improved ban output formatting

--- commands/staff/Ban.js
+++ commands/staff/Ban.js
@@ -1,55 +1,62 @@
 module.exports = Cmds.addCommand({
-    cmds: [";ban ", ";permaban "],
+    cmds: [';ban ', ';permaban '],
 
     requires: {
         guild: true,
-        loud: false
+        loud: false,
     },
 
-    desc: "Ban a user from the guild",
+    desc: 'Ban a user from the guild',
 
-    args: "([@user] | [id] | [name]) ([reason])",
+    args: '([@user] | [id] | [name]) ([reason])',
 
-    example: "vaeb being weird",
+    example: 'vaeb being weird',
 
-    ///////////////////////////////////////////////////////////////////////////////////////////
+    // /////////////////////////////////////////////////////////////////////////////////////////
 
     func: (cmd, args, msgObj, speaker, channel, guild) => {
-        var data = Util.getDataFromString(args, [
-            function(str, results) {
+        const data = Util.getDataFromString(args, [
+            function (str) {
                 return Util.getMemberByMixed(str, guild);
             },
         ], true);
-        if (!data) return Util.commandFailed(channel, speaker, "User not found");
-        var target = data[0];
-        var reason = data[1];
+        if (!data) return Util.commandFailed(channel, speaker, 'User not found');
+        const target = data[0];
+        const reason = data[1];
         if (Util.getPosition(speaker) <= Util.getPosition(target)) {
-            Util.commandFailed(channel, speaker, "User has equal or higher rank");
-            return;
+            Util.commandFailed(channel, speaker, 'User has equal or higher rank');
+            return false;
         }
-        var targName = Util.getName(target);
-        var targId = Util.safe(target.id);
-        //var dayMessages = Util.getInt(args);
+        const targName = Util.getName(target);
+        const targId = Util.safe(target.id);
+        // var dayMessages = Util.getInt(args);
 
-        var outStr = ["**You have been banned**\n```"];
-        outStr.push("Guild: " + guild.name);
-        outStr.push("Reason: " + reason);
-        outStr.push("```");
-        Util.print(target, outStr.join("\n"));
+        const outStr = ['**You have been banned**\n```'];
+        outStr.push(`Guild: ${guild.name}`);
+        outStr.push(`Reason: ${reason}`);
+        outStr.push('```');
+        Util.print(target, outStr.join('\n'));
 
         Util.banMember(target, speaker, reason);
 
-        Util.print(channel, "Banned", Util.fix(targName), "(" + targId + ") for", Util.fix(reason));
-        if (guild.id == "168742643021512705") index.dailyBans.push([targId, targName + "#" + target.discriminator, reason]);
+        const sendEmbedFields = [
+            { name: 'Username', value: target.toString() },
+            { name: 'Ban Reason', value: reason },
+        ];
+        Util.sendEmbed(channel, 'User Banned', null, Util.makeEmbedFooter(speaker), Util.getAvatar(target), 0x00E676, sendEmbedFields);
 
-        var sendLogData = [
-            "Guild Ban",
+        if (guild.id == '168742643021512705') index.dailyBans.push([targId, `${targName}#${target.discriminator}`, reason]);
+
+        const sendLogData = [
+            'Guild Ban',
             guild,
             target,
-            {name: "Username", value: target.toString()},
-            {name: "Moderator", value: speaker.toString()},
-            {name: "Ban Reason", value: reason}
+            { name: 'Username', value: target.toString() },
+            { name: 'Moderator', value: speaker.toString() },
+            { name: 'Ban Reason', value: reason },
         ];
         Util.sendLog(sendLogData, colAction);
-    }
+
+        return true;
+    },
 });
