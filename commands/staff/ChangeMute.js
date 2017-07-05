cf4780be7f04e4e818734b98899310388899cf95
Vaeb <vaebmail@gmail.com>
2017-07-05 22:27:47 +0100

    Applied new optional args to change command too

--- commands/staff/ChangeMute.js
+++ commands/staff/ChangeMute.js
@@ -8,7 +8,7 @@ module.exports = Cmds.addCommand({
 
     desc: 'Change details of an active mute',
 
-    args: '([@user] | [id] | [name]) ([mute_length_hours]) ([reason])',
+    args: '([@user] | [id] | [name]) (OPT: [mute_length]) (OPT: [mute_length_format]) (OPT: [reason])',
 
     example: 'vae 3 spamming multiple channels',
 
@@ -17,33 +17,48 @@ module.exports = Cmds.addCommand({
     func: (cmd, args, msgObj, speaker, channel, guild) => {
         args = args.trim();
 
-        const data = Util.getDataFromString(args, [
-            function (str) {
-                return Util.getMemberByMixed(str, guild) || Util.isId(str);
-            },
-        ], true);
+        const data = Util.getDataFromString(args,
+            [
+                [
+                    function (str) {
+                        return Util.getMemberByMixed(str, guild) || Util.isId(str);
+                    },
+                ],
+                [
+                    function (str) {
+                        const timeHours = Util.matchWholeNumber(str);
+                        return timeHours;
+                    },
+                ],
+                [
+                    function (str) {
+                        let mult;
+                        str = str.toLowerCase();
+                        if (str.substr(str.length - 1, 1) == 's' && str != 'ms' && str != 's') str = str.substr(0, str.length - 1);
+                        if (str == 'millisecond' || str == 'ms') mult = 1 / 60 / 60 / 1000;
+                        if (str == 'second' || str == 's' || str == 'sec') mult = 1 / 60 / 60;
+                        if (str == 'minute' || str == 'm' || str == 'min') mult = 1 / 60;
+                        if (str == 'hour' || str == 'h') mult = 1;
+                        if (str == 'day' || str == 'd') mult = 24;
+                        if (str == 'week' || str == 'w') mult = 24 * 7;
+                        if (str == 'month' || str == 'mo') mult = 24 * 30.42;
+                        if (str == 'year' || str == 'y') mult = 24 * 365.2422;
+                        return mult;
+                    },
+                ],
+            ]
+        , true);
 
         if (!data) {
             return Util.sendEmbed(channel, 'Mute Failed', 'User not found', Util.makeEmbedFooter(speaker), null, 0x00E676, null);
         }
 
-        const data2 = Util.getDataFromString(data[1], [
-            function (str) {
-                const timeHours = Util.matchWholeNumber(str);
-                return timeHours;
-            },
-        ], true);
+        console.log(`Change Arg Data: ${data}`);
 
         const member = data[0];
-        let time;
-        let reason;
-
-        if (data2) {
-            time = Number(data2[0]) * 1000 * 60 * 60;
-            reason = data2[1];
-        } else {
-            reason = data[1];
-        }
+        const mult = data[2] || 1;
+        const time = data[1] ? data[1] * 1000 * 60 * 60 * mult : null;
+        const reason = data[3];
 
         Mutes.changeMute(guild, channel, member, speaker, { 'time': time, 'reason': reason });
 
