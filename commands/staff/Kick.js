e9ea4136102f33b405eb627a56dab1f6a9ab75e7
Vaeb <vaebmail@gmail.com>
2017-07-22 23:05:51 +0100

    Kick perms

--- commands/staff/Kick.js
+++ commands/staff/Kick.js
@@ -1,54 +1,57 @@
 module.exports = Cmds.addCommand({
-    cmds: [";kick ", ";eject "],
+    cmds: [';kick ', ';eject '],
 
     requires: {
         guild: true,
-        loud: false
+        loud: false,
     },
 
-    desc: "Kick a user from the guild",
+    desc: 'Kick a user from the guild',
 
-    args: "([@user] | [id] | [name]) ([reason])",
+    args: '([@user] | [id] | [name]) ([reason])',
 
-    example: "vaeb being weird",
+    example: 'vaeb being weird',
 
-    ///////////////////////////////////////////////////////////////////////////////////////////
+    // /////////////////////////////////////////////////////////////////////////////////////////
 
     func: (cmd, args, msgObj, speaker, channel, guild) => {
-        var data = Util.getDataFromString(args, [
-            function(str, results) {
+        if (speaker.id == '138274235435974656') return Util.commandFailed(channel, speaker, 'Temporarily disabled kick permissions for this moderator as a precaution due to complaints');
+        const data = Util.getDataFromString(args, [
+            function (str) {
                 return Util.getMemberByMixed(str, guild);
             },
         ], true);
-        if (!data) return Util.commandFailed(channel, speaker, "User not found");
-        var target = data[0];
-        var reason = data[1];
+        if (!data) return Util.commandFailed(channel, speaker, 'User not found');
+        const target = data[0];
+        const reason = data[1];
         if (Util.getPosition(speaker) <= Util.getPosition(target)) {
-            Util.commandFailed(channel, speaker, "User has equal or higher rank");
-            return;
+            Util.commandFailed(channel, speaker, 'User has equal or higher rank');
+            return false;
         }
-        var targName = Util.getName(target);
-        var targId = Util.safe(target.id);
+        const targName = Util.getName(target);
+        const targId = Util.safe(target.id);
 
-        var outStr = ["**You have been kicked**\n```"];
-        outStr.push("Guild: " + guild.name);
-        outStr.push("Reason: " + reason);
-        outStr.push("```");
-        Util.print(target, outStr.join("\n"));
+        const outStr = ['**You have been kicked**\n```'];
+        outStr.push(`Guild: ${guild.name}`);
+        outStr.push(`Reason: ${reason}`);
+        outStr.push('```');
+        Util.print(target, outStr.join('\n'));
 
         Util.kickMember(target, speaker, reason);
 
-        Util.print(channel, "Kicked", Util.fix(targName), "(" + targId + ") for", Util.fix(reason));
-        if (guild.id == "168742643021512705") index.dailyKicks.push([targId, targName + "#" + target.discriminator, reason]);
+        Util.print(channel, 'Kicked', Util.fix(targName), `(${targId}) for`, Util.fix(reason));
+        if (guild.id == '168742643021512705') index.dailyKicks.push([targId, `${targName}#${target.discriminator}`, reason]);
 
-        var sendLogData = [
-            "Guild Kick",
+        const sendLogData = [
+            'Guild Kick',
             guild,
             target,
-            {name: "Username", value: target.toString()},
-            {name: "Moderator", value: speaker.toString()},
-            {name: "Kick Reason", value: reason}
+            { name: 'Username', value: target.toString() },
+            { name: 'Moderator', value: speaker.toString() },
+            { name: 'Kick Reason', value: reason },
         ];
         Util.sendLog(sendLogData, colAction);
-    }
+
+        return true;
+    },
 });
