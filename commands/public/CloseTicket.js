09d1668a4687fbfce0c269708058c1d812aa531e
Vaeb <vaebmail@gmail.com>
2017-08-14 22:45:39 +0100

    Ticket updates

--- commands/public/CloseTicket.js
+++ commands/public/CloseTicket.js
@@ -42,6 +42,7 @@ module.exports = Cmds.addCommand({
 
         const sendEmbedFields = [
             { name: 'Ticket User', value: `<@${foundTicket.user_id}>`, inline: false },
+            { name: 'Ticket Info', value: `${foundTicket.description}`, inline: false },
             { name: 'Ticket Opened', value: Util.getDateString(new Date(foundTicket.open_tick)), inline: false },
         ];
         Util.sendEmbed(channel, `Closed Ticket #${foundTicket.ticket_id}`, null, Util.makeEmbedFooter(speaker), null, colGreen, sendEmbedFields);
