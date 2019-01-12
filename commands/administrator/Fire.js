b6bf36cacd0b10a40445a999ca38ef850f8a0dc7
Vaeb <vaebmail@gmail.com>
2019-01-12 16:33:59 +0000

    random

---
+++ commands/administrator/Fire.js
@@ -0,0 +1,32 @@
+module.exports = Cmds.addCommand({
+    cmds: [';fire '],
+
+    requires: {
+        guild: true,
+        loud: false,
+    },
+
+    desc: 'Set fire to the server',
+
+    args: '(on | off)',
+
+    example: 'on',
+
+    // /////////////////////////////////////////////////////////////////////////////////////////
+
+    func: (cmd, args, msgObj, speaker, channel, guild) => {
+        if (!args[0] || !/on|true|off|false/i.test(args[0])) return;
+
+        const isOn = /on|true/i.test(args[0]);
+
+        if (isOn) {
+            guild.channels.forEach((c) => {
+                if (!c.name.endsWith('🔥')) c.setName(`${c.name}🔥`);
+            });
+        } else {
+            guild.channels.forEach((c) => {
+                if (c.name.endsWith('🔥')) c.setName(c.name.substr(0, c.name.length - 1));
+            });
+        }
+    },
+});
