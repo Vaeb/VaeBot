d077ef10e094f8a01b7933e494085411b6402772
Vaeb <vaebmail@gmail.com>
2019-02-11 21:29:30 +0000

    always tts

--- commands/administrator/RaveBan.js
+++ commands/administrator/RaveBan.js
@@ -63,7 +63,7 @@ module.exports = Cmds.addCommand({
         let count = 0;
 
         const intervalFunc = () => {
-            channel.send(`🦀 ${target} IS GONE 🦀`, { tts: count++ % 2 == 0, files: [crabRaveGif] }).catch(console.error);
+            channel.send(`🦀 ${target} IS GONE 🦀`, { tts: count++ % 1 == 0, files: [crabRaveGif] }).catch(console.error);
         };
 
         index.crabRave.interval = setInterval(intervalFunc, 5000);
