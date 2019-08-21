d4a734db3e6508912b0b692c7b407fc5c5b729f5
Vaeb <vaebmail@gmail.com>
2019-08-21 21:07:46 +0100

    Fix error check v2

--- commands/public/Translate.js
+++ commands/public/Translate.js
@@ -14,7 +14,7 @@ module.exports = Cmds.addCommand({
 
     func: (cmd, args, msgObj, speaker, channel, guild) => {
         index.Translate.translate(args, 'en', (err, res) => {
-            if (err && err.error) return console.log(err.error);
+            if (err) return console.log(err);
 
             const embFields = [
                 { name: `[${res.detectedSourceLanguage}] Original`, value: res.originalText || args, inline: false },
