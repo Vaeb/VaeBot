84c6a829f36f43f63bbf1810c679dbada1b454d0
Vaeb <vaebmail@gmail.com>
2019-08-21 20:52:42 +0100

    Translate err check

--- commands/public/Translate.js
+++ commands/public/Translate.js
@@ -13,11 +13,14 @@ module.exports = Cmds.addCommand({
     example: 'Hola mis amigos',
 
     func: (cmd, args, msgObj, speaker, channel, guild) => {
-        index.translate(args, 'en', (err, res) => {
+        index.Translate.translate(args, 'en', (err, res) => {
+            if (err) return console.error(err);
+
             const embFields = [
                 { name: `[${res.detectedSourceLanguage}] Original`, value: res.originalText || args, inline: false },
                 { name: '[en] Translation', value: res.translatedText, inline: false },
             ];
+
             Util.sendEmbed(channel, 'Translated', null, Util.makeEmbedFooter(speaker), null, colGreen, embFields);
         });
 
